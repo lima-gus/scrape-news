e1f2ea1be203b56db895d22b7c8d00b94be0734f
Gustavo Lima <lima_gus@outlook.com>
2022-07-25 18:13:26 -0300

    feat: scrape_novidades

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from parsel import Selector
 
 
 # Requisito 1
@@ -25,6 +26,8 @@ def fetch(url):
 # Requisito 2
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css('a.cs-overlay-link::attr(href)').getall()
 
 
 # Requisito 3

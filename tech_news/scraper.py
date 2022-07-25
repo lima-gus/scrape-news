8483fd2834f46bb7b84fa9b0ee8d756db33d679a
Gustavo Lima <lima_gus@outlook.com>
2022-07-25 18:05:06 -0300

    feat: fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,25 @@
+import time
+import requests
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(
+            url,
+            headers={"user-agent": "Fake user-agent"},
+            timeout=3
+        )
+
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+
+    except (requests.ReadTimeout, requests.HTTPError):
+        return None
 
 
 # Requisito 2

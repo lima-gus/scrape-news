9bd9585f79cfb4fb0729696b86cdd8dca8898856
Gustavo Lima <lima_gus@outlook.com>
2022-07-25 19:56:29 -0300

    feat: scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -40,6 +40,38 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+
+    title = selector.css('h1.entry-title::text').get().strip()
+
+    timestamp = selector.css('li.meta-date::text').get()
+
+    writer = selector.css('span.author a::text').get().strip()
+
+    comments_count = selector.css(
+        'h5.title-block ol.comment-list li'
+        ).getall().__len__() or 0
+
+    summary = ''.join(selector.css(
+        'div.entry-content p'
+        )[0].xpath('.//text()').getall()).strip()
+
+    tags = selector.css('section.post-tags ul li a::text').getall()
+
+    category = selector.css('a.category-style span.label::text').get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category
+    }
 
 
 # Requisito 5

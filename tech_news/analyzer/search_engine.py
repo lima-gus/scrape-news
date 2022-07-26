f9e939500a63291b25d251dab407f8b63b5fee6b
Gustavo Lima <lima_gus@outlook.com>
2022-07-26 18:11:33 -0300

    feat: search_by_tag

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -21,6 +21,13 @@ def search_by_date(date):
 # Requisito 8
 def search_by_tag(tag):
     """Seu código deve vir aqui"""
+    news_by_tag = []
+    news = search_news({'tags': {'$regex': tag, '$options': '-i'}})
+
+    for new in news:
+        news_by_tag.append((new['title'], new['url']))
+
+    return news_by_tag
 
 
 # Requisito 9
